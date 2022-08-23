e39fe93733b4f739f4a8a2a34a0fe8c4d0923bcb
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2022-08-23 10:36:19 +0200

    tab2

--- ionic-wordle/src/app/tab2/tab2.page.ts
+++ ionic-wordle/src/app/tab2/tab2.page.ts
@@ -7,6 +7,117 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  actualResult='AUDIO';
 
+  keyboad=[
+    {key: 'Q', class: ''},
+    {key: 'W', class: ''},
+    {key: 'E', class: ''},
+    {key: 'R', class: ''},
+    {key: 'T', class: ''},
+    {key: 'Y', class: ''},
+    {key: 'U', class: ''},
+    {key: 'I', class: ''},
+    {key: 'O', class: ''},
+    {key: 'P', class: ''},
+    {key: 'A', class: ''},
+    {key: 'S', class: ''},
+    {key: 'D', class: ''},
+    {key: 'F', class: ''},
+    {key: 'G', class: ''},
+    {key: 'H', class: ''},
+    {key: 'J', class: ''},
+    {key: 'K', class: ''},
+    {key: 'L', class: ''},
+    {key: 'ENTER', class: ''},
+    {key: 'Z', class: ''},
+    {key: 'X', class: ''},
+    {key: 'C', class: ''},
+    {key: 'V', class: ''},
+    {key: 'B', class: ''},
+    {key: 'N', class: ''},
+    {key: 'M', class: ''},
+    {key: 'BACKSPACE', class: ''}
+  ];
+
+
+  boxes=[
+    [{class:'', key: ''},{class:'', key: ''},{class:'', key: ''},{class:'', key: ''},{class:'', key: ''}],
+    [{class:'', key: ''},{class:'', key: ''},{class:'', key: ''},{class:'', key: ''},{class:'', key: ''}],
+    [{class:'', key: ''},{class:'', key: ''},{class:'', key: ''},{class:'', key: ''},{class:'', key: ''}],
+    [{class:'', key: ''},{class:'', key: ''},{class:'', key: ''},{class:'', key: ''},{class:'', key: ''}],
+    [{class:'', key: ''},{class:'', key: ''},{class:'', key: ''},{class:'', key: ''},{class:'', key: ''}],
+    [{class:'', key: ''},{class:'', key: ''},{class:'', key: ''},{class:'', key: ''},{class:'', key: ''}],
+  ];
+
+  rowIndex=0;
+  columnIndex=0;
+
+  handleChange(pressed: any){
+  // backspace
+  if (pressed === 'BACKSPACE') {
+    if(this.columnIndex > 0) {
+      this.clearKeyIndex();
+    }
+    return;
+  }
+  // enter
+  if (pressed === 'ENTER') {
+    this.submitData();
+    return;
+  }
+  // normal
+    console.log(pressed);
+    if(this.columnIndex < 5 && this.rowIndex < 6) {
+      this.boxes[this.rowIndex][this.columnIndex]={class:'', key: pressed};
+      console.log({box: this.boxes});
+      this.columnIndex++;
+    }
+  }
+
+  clearKeyIndex() {
+    this.boxes[this.rowIndex][this.columnIndex - 1]={class:'', key: ''};
+    this.columnIndex--;
+    console.log({currentIndex: this.columnIndex, box: this.boxes});
+  }
+
+  submitData(){
+
+    let clonedCopyOfActualGuess = this.actualResult;
+
+    // CORRECT WORD
+    console.log(`data submitted, ENTER pressed`);
+    if (this.columnIndex === 5 && this.rowIndex < 6) {
+      const guessedLetters = this.boxes[this.rowIndex].map((item) => {return item.key});
+      const guessedWord = guessedLetters.join('');
+      console.log({guessedWord});
+      if (this.actualResult === guessedWord) {
+        alert('Congratulation!');
+        return;
+      }
+
+      // colors
+      this.boxes[this.rowIndex].map((item, index) => {
+        if(item.key === this.actualResult[index]) {
+          item.class=`limegreen`;
+          clonedCopyOfActualGuess = clonedCopyOfActualGuess.replace(item.key, '');
+        }
+      });
+
+      this.boxes[this.rowIndex].map((item, index) => {
+        if(clonedCopyOfActualGuess.includes(item.key)) {
+          item.class=`gold`;
+        }
+      });
+
+      this.boxes[this.rowIndex].map((item, index) => {
+        if(item.class === '') {
+          item.class=`grey`;
+        }
+      });
+
+      console.log({boxes: (this.boxes)});
+
+    }
+  }
 }
