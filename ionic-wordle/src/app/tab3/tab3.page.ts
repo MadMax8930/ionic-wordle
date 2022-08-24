600c820163aae6f73975e7f98e1022e386f690ff
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2022-08-24 12:11:24 +0200

    tab3 done

--- ionic-wordle/src/app/tab3/tab3.page.ts
+++ ionic-wordle/src/app/tab3/tab3.page.ts
@@ -212,20 +212,6 @@ export class Tab3Page {
     }
 
     // Check if the current try is a word in the list.
-    const wordFromCurTry =
-        curTry.letters.map(letter => letter.text).join('').toUpperCase();
-    if (!WORDS.includes(wordFromCurTry)) {
-      this.showInfoMessage('Not in word list');
-      // Shake the current row.
-      const tryContainer =
-          this.tryContainers.get(this.numSubmittedTries)?.nativeElement as
-          HTMLElement;
-      tryContainer.classList.add('shake');
-      setTimeout(() => {
-        tryContainer.classList.remove('shake');
-      }, 500);
-      return;
-    }
 
     // Check if the current try matches the target word.
 
@@ -307,11 +293,6 @@ export class Tab3Page {
       this.showInfoMessage('NICE!');
       this.won = true;
       // Bounce animation.
-      for (let i = 0; i < letterEles.length; i++) {
-        const curLetterEle = letterEles[i];
-        curLetterEle.classList.add('bounce');
-        await this.wait(160);
-      }
       this.showShare();
       return;
     }
