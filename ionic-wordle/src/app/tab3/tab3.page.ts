8da1e48a4618703a81c464b9f04d40515018c53e
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2022-08-25 16:43:54 +0200

    tab3 db

--- ionic-wordle/src/app/tab3/tab3.page.ts
+++ ionic-wordle/src/app/tab3/tab3.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ViewEncapsulation } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 export interface Data {
   statistics: string;
@@ -16,16 +17,17 @@ export class Tab3Page {
   public data: Data;
   public columns: any;
   public rows: any;
+
+  baseUrl: string = environment.urlApi;
   constructor(private http: HttpClient) {
     this.columns = [
-      { name: 'Word' },
-      { name: 'Tries' },
-      { name: 'Guessed' }
+      { name: 'Id' },
+      { name: 'Content' },
     ];
-    this.http.get<Data>('../../assets/tries.json')
+    this.http.get<Data>(this.baseUrl + '/word')
       .subscribe((res) => {
         console.log(res);
-        this.rows = res.statistics;
+        this.rows = res;
       });
   }
 }
